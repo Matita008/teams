03d45f2b3c9c2dc4db897e5959243070311a533c
Matita008 <155455608+Matita008@users.noreply.github.com>
2025-12-28 19:00:34 +0100

    start

---
+++ src/main/java/io/matita08/plugins/teams/Teams.java
@@ -0,0 +1,17 @@
+package io.matita08.plugins.teams;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Teams extends JavaPlugin {
+   
+   @Override
+   public void onEnable() {
+      // Plugin startup logic
+      
+   }
+   
+   @Override
+   public void onDisable() {
+      // Plugin shutdown logic
+   }
+}
